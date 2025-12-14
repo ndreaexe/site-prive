2bfc1084b0eefd6d4aaeded5c4275d82b20c2115
ndreaexe <henriot70200@gmail.com>
2025-12-14 16:26:53 +0100

    test flask direct

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "TEST FLASK DIRECT 🔥"
+

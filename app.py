2c229cbb2dd900b141042f21c695b9f1c9bf42cd
ndreaexe <henriot70200@gmail.com>
2025-12-14 15:44:07 +0100

    Ajout route principale

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/monliensecret123")
-def secret():
-    return "<h1>Bienvenue sur mon site privé 🔐</h1>"
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def home():
+    return "Mon site est en ligne 🚀"

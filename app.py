2a185f7edef57a535358566301e2e014f385dec1
ndreaexe <henriot70200@gmail.com>
2025-12-14 16:13:45 +0100

    utilisation page html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "SITE OK 🚀"
+    return render_template("index.html")
